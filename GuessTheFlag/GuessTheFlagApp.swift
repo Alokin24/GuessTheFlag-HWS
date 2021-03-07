93ef42845b3d3005ca85ef2a644443befd64d714
Nikola Anastasovski <nikola.anastasovski@netcetera.com>
2021-03-07 16:02:11 +0100

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Nikola Anastasovski on 7.3.21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
